c62851ab560d4381c88543ffa2b3644d160d40ae
Meteodeep <dobg1.91@gmail.com>
2017-03-11 21:53:53 +0000

    Weather Warning
    Comments

--- WeatherWarning.py
+++ WeatherWarning.py
@@ -1,15 +1,15 @@
-from time import sleep
+from time import sleep  #sleep functions
 	
-from sense_hat import SenseHat
+from sense_hat import SenseHat  #imports sense hat
 
 sense = SenseHat()
 sense.clear()
 
-pressure = sense.get_pressure()
+pressure = sense.get_pressure()  #receives pressure
 pressure = round(pressure, 1)
 pressure = str(pressure)
 
-if pressure <= "995":
+if pressure <= "995":   #if statement
     target1 = open('/var/www/meteodeep/weatherwarning.txt', 'w')
     target1.write(pressure)
     target1.close()
@@ -23,7 +23,7 @@ else:
     print("ERROR")
 
 
-from subprocess import call
+from subprocess import call  #dropbox upload
 Upload = "/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /var/www/meteodeep/weatherwarning.txt /WeatherWarning.txt"
 call ([Upload], shell=True)
 
